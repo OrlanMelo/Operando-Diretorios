f3f317e5c339dc6038d7d10dbbb39c4740fec1b5
Orlan Melo Lobeu <142623736+OrlanMelo@users.noreply.github.com>
2023-10-07 15:35:15 -0300

    Código
    Manipulando e efetuando tarefas em diretórios.
    Nestas tarefas, constam 4 etapas.
    
    - Criando um diretório.
    - Criando um diretório e subdiretórios sequentes.
    - Renomeando diretório.
    - Deletando diretório e subdiretórios sequentes.

---
+++ OperandoDiretorios.cpp
@@ -0,0 +1,87 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+
+using namespace std;
+using namespace filesystem;
+
+class cFuncoes
+{
+private:
+public:
+
+	void CriarDiretorio(path& Diretorio)
+	{
+		try
+		{
+			if (create_directory(Diretorio) == true)
+				cout << "O diretório foi criado com sucesso.\n";
+			else
+				cout << "Não foi possível criar o diretório..";
+		}
+		catch (filesystem_error& erro)
+		{
+			cout << erro.what();
+		}
+	}
+
+	void CriarArvoreDeDiretorios(path& Arvore)
+	{
+		try
+		{
+			if (create_directories(Arvore) == true)
+				cout << "O diretório principal e a árvore foi criada.\n";
+			else
+				cout << "Não foi possível criar a árvore de diretórios..";
+		}
+		catch (filesystem_error& erro)
+		{
+			cout << erro.what();
+		}
+	}
+
+	void RenomearDiretorio(path& Antes, path& Novo)
+	{
+		try
+		{
+			rename(Antes, Novo);
+		}
+		catch (filesystem_error& erro)
+		{
+			cout << erro.what();
+		}
+	}
+
+	void DeletarDiretorio(path& Diretorio, bool IncluirSubdiretorios)
+	{
+		try
+		{
+			uintmax_t max;
+			error_code codigo;
+
+			if (IncluirSubdiretorios == true)
+				_Remove_all_dir(Diretorio, codigo, max);
+			else
+				remove(Diretorio);
+		}
+		catch (filesystem_error& erro)
+		{
+			cout << erro.what();
+		}
+	}
+
+}Funcoes;
+
+int main()
+{
+
+	cout << "O assistente está efetuando manipulações e tarefas no diretório...";
+
+	Funcoes.CriarDiretorio((path)"C:\\teste2");
+	Funcoes.CriarArvoreDeDiretorios((path)"C:\\Pai/subdir1/subdir2");
+	Funcoes.RenomearDiretorio((path)"C:\\teste2", (path)"C:\\teste3");
+	Funcoes.DeletarDiretorio((path)"C:\\teste3", false);
+
+	system("pause");
+}
